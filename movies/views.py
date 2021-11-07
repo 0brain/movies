d8318bc6f8876498c3923b3eaff291b6d16ec0f7
nazar <nazar.khort@gmail.com>
2021-11-07 20:05:52 +0200

    Created MovieListSerializer to serialize fields ("title", "tagline", "category") which will be send in json format. Also created MovieListView which will send serialized data. In urls added a path to list of movies.

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieListSerializer
+
+
+class MovieListView(APIView):
+    """Вывод списка фильмов"""
+    def get(self, request):
+        movies = Movie.objects.filter(draft=False)
+        serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data)
