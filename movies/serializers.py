f4258f5cbb25eda03b1ba68a0e02d79e24acfdcd
nazar <nazar.khort@gmail.com>
2021-11-07 20:17:22 +0200

    Created MovieDetailSerializer to serialize all fields except 'draft', which will be send in json format. Also created MovieDetailView which will send serialized data. In urls added a path to single movie.

--- movies/serializers.py
+++ movies/serializers.py
@@ -9,3 +9,11 @@ class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ("title", "tagline", "category")
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    """Полный фильм"""
+
+    class Meta:
+        model = Movie
+        exclude = ("draft", )
