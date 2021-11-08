f14b53b24d333763bb7c248393e0dec2b200f807
nazar <nazar.khort@gmail.com>
2021-11-08 22:22:42 +0200

    Created ReviewSerializer to display comments. In Review model added related_name="reviews". In MovieDetailSerializer added reviews, which is based on ReviewSerializer. It displays list of comments related to certain movie.

--- movies/serializers.py
+++ movies/serializers.py
@@ -11,21 +11,32 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ("title", "tagline", "category")
 
 
-class MovieDetailSerializer(serializers.ModelSerializer):
-    """Повна інформація про фільм"""
-    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
-    directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
-    actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
-    genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
-
-    class Meta:
-        model = Movie
-        exclude = ("draft", )
-
-
 class ReviewCreateSerializer(serializers.ModelSerializer):
     """Додавання коментаря"""
 
     class Meta:
         model = Review
         fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Виведення коментарів"""
+
+    class Meta:
+        model = Review
+        fields = ("name", "text", "parent")
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    """Повна інформація про фільм"""
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    reviews = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        exclude = ("draft", )
+
+
